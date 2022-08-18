b5c16b3094757ca4b2403940c12693828ca91218
Sasha_Nikonorov <i_sasha_n@mail.ru>
2022-08-18 19:43:03 +0300

    modifed program:add function

--- dz_z25/Program.cs
+++ dz_z25/Program.cs
@@ -1,8 +1,20 @@
-﻿Console.WriteLine("Введите число:");
+﻿
+int AdegreeB(int A, int B)
+{
+
+    int result = 1;
+
+    for (var index = 1; index <= B; index++)
+        result = result*A;
+
+    return result;
+
+}
+
+Console.WriteLine("Введите число:");
 int A = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите число, для возведения А в натуральную степень B:");
 int B = int.Parse(Console.ReadLine()!);
 
-var result = Math.Pow(A, B);
-
+int result = AdegreeB(A, B);
 Console.WriteLine(result);
