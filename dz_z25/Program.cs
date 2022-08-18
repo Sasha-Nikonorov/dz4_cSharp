e24ca73327538ab434db92b1915f43c32a4e1326
Sasha_Nikonorov <i_sasha_n@mail.ru>
2022-08-18 15:06:09 +0300

    raises A natural degree B

---
+++ dz_z25/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число:");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число, для возведения А в натуральную степень B:");
+int B = int.Parse(Console.ReadLine()!);
+
+var result = Math.Pow(A, B);
+
+Console.WriteLine(result);
