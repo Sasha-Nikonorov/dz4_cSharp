cfe50695baf0dbe8b6af8973490ffdc4ea2be6b0
Sasha_Nikonorov <i_sasha_n@mail.ru>
2022-08-19 22:22:12 +0300

    array

---
+++ dz_z29/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите размер массива");
+int X = int.Parse(Console.ReadLine()!);
+
+int[] createArray()
+{
+    var random = new Random();
+
+    var result = new int[X];
+
+    for (long i = 0; i < result.Length; i++)
+    {
+        result[i] = random.Next(-9, 10);
+    }
+
+    return result;
+}
+
+void printArray1(int[] array)
+{
+    Console.Write("[");
+
+    for (var i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+
+        if (i != array.Length - 1)
+            Console.Write(", ");
+    }
+
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+var array = createArray();
+printArray1(array);
